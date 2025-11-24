e5fe70c256aceada92ddeb8cbc631c2cc1ff0a8e
Ibtissamlot <ibtissamlotfi@hotmail.com>
2025-11-24 21:17:05 +0100

    Update run_etl.sh

--- run_etl.sh
+++ run_etl.sh
@@ -1,4 +1,8 @@
-export PYTHONPATH=$PYTHONPATH:/app/src
+import sys
+import os
+# Ajouter le répertoire parent de 'etl' au chemin Python
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 #!/bin/bash
 
 # Se déplacer dans le répertoire de travail
